2d5b6bc0c35cba53af6b088bc3c35af64446111a
vkriskanth <vkriskanth@gmail.com>
2024-05-30 20:47:38 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Project Name: <project_name>
+
+### Overview
+
+This project uses Llama3 to generate unit test cases for code. With Llama3, you can easily create and manage test suites, tests, and fixtures for your Coding Projects.
+
+### Features
+
+* Generates unit test cases using Llama3
+* Supports 
+* 1. Python
+2. Java
+3. C#
+4. JavaScript
+5. Ruby
+6. Go
+7. Swift
+
+* Easy to use and integrate into your project workflow
+
+### How to Use
+
+1. Install Llama3 on your PC or Mac.
+2. pip install -r requirements.txt
+3. Generate texts using python main.py <language> <code_file> -> python main.py java PrimeChecker.java
+
+
+### Contributing
+
+* Fork this repository to create your own branch
+* Make changes and add new features as needed
+* Push your changes back to your forked repository
+* Submit a pull request for review and merge
+
+### License
+
+This project is licensed under the MIT License. See `LICENSE` file for details.
+
+### Credits
+
+* Llama3: The testing framework used in this project (https://github.com/<llama3_author>/llama3)
+* Python: The programming language used to develop this project (https://www.python.org/)
---
+++ main.py
@@ -0,0 +1,60 @@
+import sys
+import requests
+import json
+url = "http://localhost:11434/api/chat"
+
+def llama3(prompt):
+    data = {
+        "model": "llama3",
+        "messages": [
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        "stream": False
+    }
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.post(url, headers=headers, json=data)
+    return (response.json()['message']['content'])
+
+# code= """
+# import java.util.Scanner;
+# public class PrimeChecker {
+#     public static void main(String[] args) {
+#         Scanner scanner = new Scanner(System.in);
+#         System.out.println("Enter a number: ");
+#         int num = scanner.nextInt();
+#         if (isPrime(num)) {
+#             System.out.println(num + " is a prime number.");
+#         } else {
+#             System.out.println(num + " is not a prime number.");
+#         }
+#     }
+#     public static boolean isPrime(int n) {
+#         if (n <= 1) {
+#             return false;
+#         }
+#         for (int i = 2; i * i <= n; i++) {
+#             if (n % i == 0) {
+#                 return false;
+#             }
+#         }
+#         return true;
+#     }
+# }
+# """
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <file_name>")
+        sys.exit(1)
+    code_file = sys.argv[1]
+    language = sys.argv[2]
+    with open(code_file,'r') as file:
+        file_contents = file.read()
+    prompt = f"Generate unit test cases for the following {language} program: {file_contents}"
+    response = llama3(prompt)
+    print(response)
